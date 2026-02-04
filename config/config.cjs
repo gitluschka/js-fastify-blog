885dab35e38b3df27f45bd2d4b34eeb87e1fa901
aleksandrov <egor-ask@yandex.ru>
2026-02-04 17:38:04 +0300

    Dockerize js-fastify-blog

--- config/config.cjs
+++ config/config.cjs
@@ -1,12 +1,18 @@
 require('dotenv').config();
 
 module.exports = {
-  production: {
-    dialect: 'postgres',
-    database: process.env.POSTGRES_DB,
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    port: 5432,
-    host: process.env.DATABASE_HOST,
-  },
+  production: process.env.DATABASE_URL
+    ? {
+        dialect: 'postgres',
+        url: process.env.DATABASE_URL,
+      }
+    : {
+        dialect: 'postgres',
+        database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        port: 5432,
+        host: process.env.DATABASE_HOST,
+      },
 };
+
